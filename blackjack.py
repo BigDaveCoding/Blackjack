d84d150919b3f50d31185a0bd6fcd10f814b6edf
BigDaveCoding <bigdavecoding@gmail.com>
2023-11-17 18:02:58 +0000

    got rid of the dealer class and using a player class to create the dealer with a boolean set to True if it is the dealer. added a deal cards function in the player class which wasnt working as intended to begin with. it would return both the random cards dealt into the player and dealers list rather than one in each. modified the constructor to say None for the cards ad this fixed the problem. created a function in the CardsInPlay class which retireves and returns a random card from the card dictionary. sril need to adjust this so that if cards have been drawn the values decrease and if the value is == 0 then that card will not be available.

--- blackjack.py
+++ blackjack.py
@@ -3,16 +3,22 @@ import random
 class Player:
     #creating a player class that will track various player variables. 
     #Will have various functions throughout that will control the flow of the game
-    def __init__(self, name, money = 100, bet_amount = 0, cards = [], rounds_won = 0, rounds_lost = 0):
+    def __init__(self, name, money = 100, bet_amount = 0, cards = None, rounds_won = 0, rounds_lost = 0, is_dealer = False):
         self.name = name
         self.money = money
         self.bet_amount = bet_amount
-        self.cards = cards
+        self.cards = cards or []
         self.rounds_won = rounds_won
         self.rounds_lost = rounds_lost
-        
+        self.is_dealer = is_dealer
+
     def __repr__(self):
         return 'You have £{money} left. Your current hand is {cards}. Rounds won: {rounds_won}. Rounds Lost: {rounds_lost}'.format(money = self.money, cards = self.cards, rounds_won=self.rounds_won, rounds_lost = self.rounds_lost)
+    
+    def deal_cards(self, card):
+        self.cards.append(card.get_random_card())
+        return "{name}'s cards: {cards}".format(name=self.name, cards=self.cards)
+
 
     def player_turn(self, turn):
         pass
@@ -21,18 +27,6 @@ class Player:
     def player_lose(self, amount):
         pass
     
-
-class Dealer:
-    #Creating a dealer class which will be used to track the dealers cards.
-
-    soft_17 = 17
-    def __init__(self, cards, rounds_won, rounds_lost):
-        self.cards = cards
-        self.rounds_won = rounds_won
-        self.rounds_lost = rounds_lost
-
-
-
 class CardsInPlay:
     #this class will assign how many decks will be used throughout the game and keep track of which cards have been played and remove them from the deck gradually.
 
@@ -42,14 +36,22 @@ class CardsInPlay:
         self.decks = decks
         self.card_amount = {key: value * decks for key, value in CardsInPlay.cards_in_deck.items()}
         self.cards_played = cards_played
-            
+
+    def get_random_card(self):
+        available_cards = list(self.card_amount.keys())
+        card = random.choice(available_cards)
+
+        return card
+    
     def __repr__(self):
         return 'There are {decks} decks in play. Here is each card and how many of each card there is: {dict}'.format(decks=self.decks, dict=self.card_amount)
     
 class PlayRound:
     pass
 
-player1 = Player('Dave', 100)
-print(player1)
-cards_in_play = CardsInPlay(random.randrange(1, 5))
-print(cards_in_play)
+cards = CardsInPlay(random.randrange(1, 5))
+dealer = Player('Dealer', 0, is_dealer = True)
+player = Player('Dave')
+
+print(player.deal_cards(cards))
+print(dealer.deal_cards(cards))
