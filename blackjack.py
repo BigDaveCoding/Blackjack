ed4c7c3e330ac4cefd7991087564c477aa944d66
BigDaveCoding <bigdavecoding@gmail.com>
2023-11-17 18:21:11 +0000

    adding comments to explain what the functions are doing

--- blackjack.py
+++ blackjack.py
@@ -15,6 +15,8 @@ class Player:
     def __repr__(self):
         return 'You have £{money} left. Your current hand is {cards}. Rounds won: {rounds_won}. Rounds Lost: {rounds_lost}'.format(money = self.money, cards = self.cards, rounds_won=self.rounds_won, rounds_lost = self.rounds_lost)
     
+    #this function uses a paameter called card which will be the class CardsInPlay.
+    #Then calls upon a funtion within that class to return a random card. random card is the added to Player class card list.
     def deal_cards(self, card):
         self.cards.append(card.get_random_card())
         return "{name}'s cards: {cards}".format(name=self.name, cards=self.cards)
@@ -37,21 +39,26 @@ class CardsInPlay:
         self.card_amount = {key: value * decks for key, value in CardsInPlay.cards_in_deck.items()}
         self.cards_played = cards_played
 
+    #this function adds cards to a list from the self.cards.amount dictionary if the value of that card is greater than 0.
+    #then chooses a random card from the available cars list and decreases the dtionary key:value by 1.
     def get_random_card(self):
-        available_cards = list(self.card_amount.keys())
+        available_cards = []
+        for key, value in self.card_amount.items():
+            if value > 0:
+                available_cards.append(key)
+               
         card = random.choice(available_cards)
-
+        self.card_amount[card] -= 1
         return card
     
     def __repr__(self):
         return 'There are {decks} decks in play. Here is each card and how many of each card there is: {dict}'.format(decks=self.decks, dict=self.card_amount)
     
-class PlayRound:
-    pass
 
 cards = CardsInPlay(random.randrange(1, 5))
 dealer = Player('Dealer', 0, is_dealer = True)
 player = Player('Dave')
 
-print(player.deal_cards(cards))
 print(dealer.deal_cards(cards))
+print(player.deal_cards(cards))
+
